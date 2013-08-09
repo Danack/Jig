d31512e7255c34ff8c3587c69405753b0936d7b2
Danack <Danack@basereality.com>
2013-08-09 03:03:27 +0100

    Latest.

--- src/Intahwebz/Jig/Tests/PlaceHolderView.php
+++ src/Intahwebz/Jig/Tests/PlaceHolderView.php
@@ -6,58 +6,58 @@ namespace Intahwebz\Jig\Tests;
 
 
 use Intahwebz\Jig\JigException;
-use Intahwebz\View;
+use Intahwebz\View\BasicView;
 use Intahwebz\Jig\Tests\JigTestException;
 
 
-class PlaceHolderView implements View {
+class PlaceHolderView extends BasicView {
 
-    private $variables = array();
-    private $boundFunctions = array();
+    //private $variables = array();
+    //private $boundFunctions = array();
 
-    /**
-     * TODO - This is smelling like an abstract class IntahwebzView
-     * @param $variable
-     * @return bool
-     */
-    function getVariable($variable) {
-        if (array_key_exists($variable, $this->variables) == true) {
-            return $this->variables[$variable];
-        }
+//    /**
+//     * TODO - This is smelling like an abstract class IntahwebzView
+//     * @param $variable
+//     * @return bool
+//     */
+//    function getVariable($variable) {
+//        if (array_key_exists($variable, $this->variables) == true) {
+//            return $this->variables[$variable];
+//        }
+//
+//        return false;
+//    }
 
-        return false;
-    }
-
-    /**
-     *
-     * TODO - should this be part of BaseTemplate as it is going to be the same for every view?
-     *
-     * @param $params
-     * @return mixed|void
-     */
-    function call($params) {
-        $functionName = array_shift($params);
-
-        if (array_key_exists($functionName, $this->boundFunctions) == true) {
-            return call_user_func_array($this->boundFunctions[$functionName], $params);
-        }
-
-        if (method_exists($this, $functionName) == false) {
-            throw new JigException("No method $functionName");
-        }
-
-        return call_user_func_array([$this, $functionName], $params);
-    }
+//
+///     *
+//     * TODO - should this be part of BaseTemplate as it is going to be the same for every view?
+//     *
+//     * @param $params
+//     * @return mixed|void
+//     */
+//    function call($params) {
+//        $functionName = array_shift($params);
+//
+//        if (array_key_exists($functionName, $this->boundFunctions) == true) {
+//            return call_user_func_array($this->boundFunctions[$functionName], $params);
+//        }
+//
+//        if (method_exists($this, $functionName) == false) {
+//            throw new JigException("No method $functionName");
+//        }
+//
+//        return call_user_func_array([$this, $functionName], $params);
+//    }
 
 
-    function isVariableSet($string){
-        return array_key_exists($string, $this->variables);
-        //return false;
-    }
-
-    function assign($variable, $value){
-        $this->variables[$variable] = $value;
-    }
+//    function isVariableSet($string){
+//        return array_key_exists($string, $this->variables);
+//        //return false;
+//    }
+//
+//    function assign($variable, $value){
+//        $this->variables[$variable] = $value;
+//    }
 
     function viewFunction($foo){
         echo "Function was called.";
